92bb152ca4f25a9c70e4511368f1c135f818426b
Palmieri Matteo <88315099+Mattron2000@users.noreply.github.com>
2023-12-05 10:39:07 +0100

    generato i getters per i campi, il toString() e scritto i costruttori

--- src/main/java/token/Token.java
+++ src/main/java/token/Token.java
@@ -1,21 +1,47 @@
 package token;
 
 public class Token {
-
+	
 	private int riga;
 	private TokenType tipo;
-	private String val;
+	private String valore;
 
-	public Token(TokenType tipo, int riga, String val) {
+	public Token(TokenType tipo, int riga, String valore) {
+		this.tipo = tipo;
+		this.riga = riga;
+		this.valore = valore;
 	}
 
 	public Token(TokenType tipo, int riga) {
+		this.tipo = tipo;
+		this.riga = riga;
+		this.valore = null;
 	}
 
-	// Getters per i campi
+	public int getRiga() {
+		return this.riga;
+	}
 
+	public TokenType getTipo() {
+		return this.tipo;
+	}
+
+	public String getValore() {
+		return this.valore;
+	}
+
+	@Override
 	public String toString() {
-		return null;
+		if (valore != null)
+			return "<" +
+					tipo +
+					", riga:" + riga +
+					", valore:" + valore +
+					'>';
+		else
+			return "<" +
+					tipo +
+					", riga:" + riga +
+					'>';
 	}
-
 }
