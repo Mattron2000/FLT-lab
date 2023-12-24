aa6747cced33c2875ab59faa58de4563faa61add
Palmieri Matteo <88315099+Mattron2000@users.noreply.github.com>
2023-12-24 19:30:36 +0100

    * refactor(TestParser.java): simplify loop in TestParser class * chore(TestParser.java): remove commented out test methods

--- src/test/java/TestParser.java
+++ src/test/java/TestParser.java
@@ -5,7 +5,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DynamicTest;
@@ -20,11 +19,10 @@ class TestParser {
 	@TestFactory
 	public Stream<DynamicTest> generateDynamicTests() {
 		String fileDynamicTestFilePath = RootFolderOfJavaProject + "dynamic_tests.txt";
-		List<DynamicTest> dynamicTests = new ArrayList<>();
+		ArrayList<DynamicTest> dynamicTests = new ArrayList<>();
 
 		try (BufferedReader br = new BufferedReader(new FileReader(fileDynamicTestFilePath))) {
-			String line;
-			while ((line = br.readLine()) != null) {
+			for (String line = br.readLine(); line != null; line = br.readLine()) {
 				String[] values = line.split(",");
 
 				assertEquals(2, values.length);
@@ -32,14 +30,12 @@ class TestParser {
 				String fileName = values[0].trim();
 				String testName = values[1].trim();
 
-				DynamicTest dynamicTest = dynamicTest(testName, () -> {
+				dynamicTests.add(dynamicTest(testName, () -> {
 					Scanner s = new Scanner(RootFolderOfJavaProject + fileName);
 					Parser p = new Parser(s);
 
 					p.parse();
-				});
-
-				dynamicTests.add(dynamicTest);
+				}));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -47,104 +43,4 @@ class TestParser {
 
 		return dynamicTests.stream();
 	}
-
-	// @Test
-	// void testCorretto1() throws FileNotFoundException, LexicalException,
-	// SyntacticException {
-
-	// }
-
-	// @Test
-	// void testCorretto2() throws FileNotFoundException, LexicalException,
-	// SyntacticException {
-	// Scanner s = new Scanner(RootFolderOfJavaProject + "testParserCorretto2.txt");
-	// Parser p = new Parser(s);
-
-	// p.parse();
-	// }
-
-	// @Test
-	// void testEcc0() throws FileNotFoundException, LexicalException,
-	// SyntacticException {
-	// Scanner s = new Scanner(RootFolderOfJavaProject + "testParserEcc_0.txt");
-	// Parser p = new Parser(s);
-
-	// p.parse();
-	// }
-
-	// @Test
-	// void testEcc1() throws FileNotFoundException, LexicalException,
-	// SyntacticException {
-	// Scanner s = new Scanner(RootFolderOfJavaProject + "testParserEcc_1.txt");
-	// Parser p = new Parser(s);
-
-	// p.parse();
-	// }
-
-	// @Test
-	// void testEcc2() throws FileNotFoundException, LexicalException,
-	// SyntacticException {
-	// Scanner s = new Scanner(RootFolderOfJavaProject + "testParserEcc_2.txt");
-	// Parser p = new Parser(s);
-
-	// p.parse();
-	// }
-
-	// @Test
-	// void testEcc3() throws FileNotFoundException, LexicalException,
-	// SyntacticException {
-	// Scanner s = new Scanner(RootFolderOfJavaProject + "testParserEcc_3.txt");
-	// Parser p = new Parser(s);
-
-	// p.parse();
-	// }
-
-	// @Test
-	// void testEcc4() throws FileNotFoundException, LexicalException,
-	// SyntacticException {
-	// Scanner s = new Scanner(RootFolderOfJavaProject + "testParserEcc_4.txt");
-	// Parser p = new Parser(s);
-
-	// p.parse();
-	// }
-
-	// @Test
-	// void testEcc5() throws FileNotFoundException, LexicalException,
-	// SyntacticException {
-	// Scanner s = new Scanner(RootFolderOfJavaProject + "testParserEcc_5.txt");
-	// Parser p = new Parser(s);
-
-	// p.parse();
-	// }
-
-	// @Test
-	// void testEcc6() throws FileNotFoundException, LexicalException,
-	// SyntacticException {
-	// Scanner s = new Scanner(RootFolderOfJavaProject + "testParserEcc_6.txt");
-	// Parser p = new Parser(s);
-
-	// p.parse();
-	// }
-
-	// @Test
-	// void testEcc7() throws FileNotFoundException, LexicalException,
-	// SyntacticException {
-	// Scanner s = new Scanner(RootFolderOfJavaProject + "testParserEcc_7.txt");
-	// Parser p = new Parser(s);
-
-	// p.parse();
-	// }
-
-	// @Test
-	// void testSoloDich() throws FileNotFoundException, LexicalException,
-	// SyntacticException {
-	// Scanner s = new Scanner(RootFolderOfJavaProject + "testSoloDichPrint1.txt");
-	// Parser p = new Parser(s);
-
-	// p.parse();
-	// }
 }
-// t = s.nextToken();
-// assertEquals(4, t.getRiga());
-// assertEquals(TokenType.EOF, t.getTipo());
-// assertEquals(null, t.getValore());
