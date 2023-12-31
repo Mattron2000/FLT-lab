78f35c495d27938f9812ff83f529ca4266805caa
Palmieri Matteo <88315099+Mattron2000@users.noreply.github.com>
2023-12-31 18:38:29 +0100

    creati altri test in versione corretta dei file testEcc...
    * chore(TestParser.java): remove unused import assertThrowsExactly
    * chore(TestParser.java): add new test cases for testParserEccCorretto files

--- src/test/java/TestParser.java
+++ src/test/java/TestParser.java
@@ -1,6 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 import java.io.BufferedReader;
