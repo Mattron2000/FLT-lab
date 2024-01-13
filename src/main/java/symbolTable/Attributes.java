2041e025e23cacde655f095a8def616307828f88
Palmieri Matteo <88315099+Mattron2000@users.noreply.github.com>
2024-01-13 19:34:59 +0100

    implementato il package symbolTable e le relative classi

---
+++ src/main/java/symbolTable/Attributes.java
@@ -0,0 +1,10 @@
+package symbolTable;
+
+import ast.LangType;
+
+/**
+ * @author Palmieri Matteo
+ */
+public class Attributes {
+	private LangType type;
+}
---
+++ src/main/java/symbolTable/SymbolTable.java
@@ -0,0 +1,40 @@
+package symbolTable;
+
+import java.util.HashMap;
+
+/**
+ * @author Palmieri Matteo
+ */
+public class SymbolTable {
+
+	private static HashMap<String, Attributes> table;
+
+	public static void init() {
+		table = new HashMap<String, Attributes>();
+	}
+
+	public static boolean enter(String id, Attributes entry) {
+		if (table.get(id) != null)
+			return false;
+		
+		table.put(id, entry);
+		return true;
+	}
+
+	public static Attributes lookup(String id) {
+		return table.get(id);
+	}
+
+	public static String toStr() {
+		StringBuilder res = new StringBuilder("~ SYMBOL TABLE ~\n");
+
+		for (HashMap.Entry<String, Attributes> entry : table.entrySet())
+			res.append(entry.getKey() + "\t" + entry.getValue() + "\n");
+
+		return res.toString();
+	}
+
+	public static int size() {
+		return table.size();
+	}
+}
